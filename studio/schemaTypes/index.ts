4a7949f9d429951b4b4828d1a53f5485ee2bdd9a
charmas3r <evansmith0115@gmail.com>
2026-01-01 12:38:43 -0800

    chore: remove Site Asset schema from Sanity Studio

--- studio/schemaTypes/index.ts
+++ studio/schemaTypes/index.ts
@@ -2,7 +2,6 @@ import post from './post'
 import author from './author'
 import category from './category'
 import blockContent from './blockContent'
-import siteAsset from './siteAsset'
 import couple from './couple'
 
-export const schemaTypes = [post, author, category, blockContent, siteAsset, couple]
+export const schemaTypes = [post, author, category, blockContent, couple]
